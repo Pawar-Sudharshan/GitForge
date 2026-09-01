39118a599b68cf1f0644c95e4d6404101d85342a
pawar-Sudharshan <pawarsudharshan47@gmail.com>
2026-09-01 14:16:10 +0530

    feat: add MongoDB configuration and database connection

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,21 @@ import CommitFile from './controllers/commit.js'
 import PushFile from './controllers/push.js'
 import PullFile from './controllers/pull.js'
 import RevertFile from './controllers/revert.js'
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from "cors"
+import mongoose from "mongoose"
+import bodyParser from "body-parser"
+import cookieParser from "cookie-parser"
+import http from "http"
+import { Server } from "socket.io"
 
-yargs(hideBin(process.argv)).command('init', 'Initialize new repo', {}, (argv) => {
+dotenv.config()
+
+
+yargs(hideBin(process.argv)).command('start', 'start new server', {}, (argv) => {
+  startServer(argv);
+}).command('init', 'Initialize new repo', {}, (argv) => {
   initRepo(argv)
 }).command('add <file>', 'Add files to stage', {}, (argv) => {
   AddFile(argv)
@@ -17,6 +30,24 @@ yargs(hideBin(process.argv)).command('init', 'Initialize new repo', {}, (argv) =
   PushFile(argv)
 }).command('pull', 'Retrieve remote repo details', {}, (argv) => {
   PullFile(argv)
-}).command('revert <commitId>', 'Revert to a previous commit', {}, (argv) => {
+}).command('revert', 'Revert to a previous commit', {}, (argv) => {
   RevertFile(argv)
 }).demandCommand(1).help().argv
+
+
+function startServer() {
+  // console.log("server is stated!");
+  const app = express();
+
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cookieParser());
+  // mongoos connection
+  mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log("MongoDB connected");
+  }).catch((err) => {
+    console.log(err);
+  });
+
+}
